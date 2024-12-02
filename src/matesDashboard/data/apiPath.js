25245945fd0fe62672c1aebd710ad53ac5a3f667
sreekanth-glitch <67911036+sreekanth-glitch@users.noreply.github.com>
2024-12-02 21:33:44 +0530

    Update apiPath.js

--- src/matesDashboard/data/apiPath.js
+++ src/matesDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
 //export const API_URI = "http://localhost:4000";
 
-export const API_URI = "https://backend-nodejs-room.onrender.com";
+export const API_URI = "https://backend-nodejs-room.onrender.com"
